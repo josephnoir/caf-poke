3b5ee4576b6915111b9b7b5311bf6a98e86f91fb
Joseph Noir <joseph.noir@gmail.com>
2016-12-02 15:22:50 +0100

    Add app to test multiplexing

--- src/caf-multiplex.cpp
+++ src/caf-multiplex.cpp
@@ -18,108 +18,103 @@ using namespace std::chrono;
 namespace {
 
 using next_atom = atom_constant<atom("next")>;
-using quit_atom = atom_constant<atom("quit")>;
+using tcp_atom = atom_constant<atom("tcp")>;
+using udp_atom = atom_constant<atom("udp")>;
 
 using clk = std::chrono::high_resolution_clock;
 using tp = std::chrono::time_point<clk>;
 
 class config : public actor_system_config {
 public:
-  uint16_t port = 0;
-  size_t interval = 1000;
-  size_t payload_size = 1024;
-  size_t num = 6;
+  // client
   std::string host = "127.0.0.1";
   std::string protocol = "udp";
+  uint16_t port = 4321;
   std::string uri_str;
+  size_t num = 6;
+  // server
+  size_t interval = 100;
   bool server_mode = false;
+  uint16_t tcp = 1234;
+  uint16_t udp = 4321;
 
   config() {
-    opt_group{custom_options_, "global"}
-    .add(interval, "interval,i", "set interval between messages in us")
-    .add(protocol, "protocol,P", "set transport protocol")
-    .add(port, "port,p", "set port")
-    .add(host, "host,H", "set host (ignored in server mode)")
-    .add(uri_str, "uri,u", "set uri (ignores host, port, protocol if set)")
+    opt_group{custom_options_, "client"}
+    .add(protocol, "protocol,P", "set transport protocol (udp)")
+    .add(port, "port,p", "set port (4321)")
+    .add(host, "host,H", "set host (127.0.0.1)")
+    .add(uri_str, "uri,U", "set uri (ignores host, port, protocol if set)")
+    .add(interval, "interval,i", "set interval between messages in ms (100)")
+    .add(num, "num,n", "sends 10^n messages (10^6)");
+    opt_group{custom_options_, "server"}
     .add(server_mode, "server-mode,s", "enable server mode")
-    .add(payload_size, "payload-size,S", "set payload size (default 1024 byte)")
-    .add(num, "num,n", "set n in 10^n for the number of messages sent");
+    .add(tcp, "tcp,t", "set TCP port (1234)")
+    .add(udp, "udp,u", "set UDP port (4321)");
   }
 };
 
-struct tracking {
-  tp start;
-  tp end;
-  std::vector<size_t> received;
-  size_t last;
-};
+size_t handle(event_based_actor* self, atom_value atm, size_t val) {
+  if (val == 0) {
+    aout(self) << "[" << to_string(atm) << "] From zero ..." << std::endl;
+  } else if (val % 1000 == 0) {
+    aout(self) << "[" << to_string(atm) << "] Incrementing " << val
+               << "." << std::endl;
+  }
+  return val + 1;
+}
 
-behavior server(stateful_actor<tracking>* self) {
+behavior server(event_based_actor* self) {
   return {
-    [=](size_t i, const vector<char>& payload) {
-      self->state.start = clk::now();
-      self->state.received.emplace_back(payload.size());
-      self->state.last = i;
-      self->become(
-        [=](size_t i, const vector<char>& payload) {
-          auto& s = self->state;
-          s.received.emplace_back(payload.size());
-          if (s.last + 1 != i) {
-            std::cerr << "Out of order message, expected " << (s.last + 1)
-                      << " but received " << i << "." << std::endl;
-          }
-          s.last = i;
-        },
-        [=](quit_atom) {
-          self->state.end = clk::now();
-          auto time =
-            duration_cast<microseconds>(self->state.end - self->state.start);
-          cout << "Received " << self->state.received.size() << " messages in "
-               << time.count() << " microseconds."
-               << "(or " << (float(time.count()) / 1000) << " ms)" << std::endl;
-          self->quit();
-        }
-      );
+    [=](tcp_atom atm, size_t i) -> size_t {
+      return handle(self, atm, i);
     },
-    [=](quit_atom) {
-      std::cerr << "This should NOT have happend." << std::endl;
-      self->quit();
+    [=](udp_atom atm, size_t i) -> size_t {
+      return handle(self, atm, i);
     }
   };
 }
 
-behavior client(event_based_actor* self, const actor& other, size_t num,
-                size_t interval, const vector<char>& payload) {
+behavior client(event_based_actor* self, const actor& other,
+                size_t limit, size_t interval, atom_value atm) {
   return {
     [=](next_atom, size_t i) {
-      if (i >= num) {
-        self->send(other, quit_atom::value);
+      self->send(other, atm, i);
+    },
+    [=](size_t i) {
+      if (i >= limit) {
+        std::cerr << "DONE" << std::endl;
         self->quit();
-        aout(self) << "Sent " << i << " messages of size " << payload.size()
-                   << "." << endl;
       } else {
-        self->send(other, i, payload);
-        self->delayed_send(self, microseconds(interval), next_atom::value,
-                           1 + i);
+        self->delayed_send(self, milliseconds(interval), next_atom::value, i);
       }
+    },
+    after(chrono::milliseconds(5 * interval)) >> [=] {
+      std::cerr << "RESTARTING" << std::endl;
+      self->send(self, next_atom::value, size_t(0));
     }
   };
 }
 
 void caf_main(actor_system& system, const config& cfg) {
   if (cfg.server_mode) {
-    auto addr = cfg.uri_str.empty()
-                ? cfg.protocol + "://0.0.0.0:" + to_string(cfg.port)
-                : cfg.uri_str;
-    auto u = io::uri::make(addr);
-    if (!u) {
-      std::cerr << "Invalid uri: '" << addr << "'." << endl;
+    cout << "Running in server mode." << endl;
+    auto tcp_addr = "tcp://0.0.0.0:" + to_string(cfg.tcp);
+    auto udp_addr = "udp://0.0.0.0:" + to_string(cfg.udp);
+    auto tcp_uri = io::uri::make(tcp_addr);
+    auto udp_uri = io::uri::make(udp_addr);
+    if (!tcp_uri) {
+      std::cerr << "Invalid uri: '" << tcp_addr << "'." << endl;
       return;
     }
-    cout << "Running in server mode." << endl;
+    if (!udp_uri) {
+      std::cerr << "Invalid uri: '" << udp_addr << "'." << endl;
+      return;
+    }
     auto srvr = system.spawn(server);
-    auto port = system.middleman().publish(srvr, *u);
-    std::cout << "Open on port " << port << "." << std::endl;
+    auto port = system.middleman().publish(srvr, *tcp_uri);
+    std::cout << "Open on TCP port " << port << "." << std::endl;
+    port = system.middleman().publish(srvr, *udp_uri);
+    std::cout << "Open on UDP port " << port << "." << std::endl;
     system.await_all_actors_done();
   } else {
     auto addr = cfg.uri_str.empty()
@@ -136,10 +131,8 @@ void caf_main(actor_system& system, const config& cfg) {
       std::cerr << "Failed to contact server on '" << addr << "'." << std::endl;
       return;
     }
-    vector<char> payload(cfg.payload_size);
-    std::iota(begin(payload), end(payload), char(0));
-    auto clnt = system.spawn(client, *srvr, pow(10, cfg.num),
-                             cfg.interval, payload);
+    auto atm = atom_from_string(caf::io::to_string(u->scheme()));
+    auto clnt = system.spawn(client, *srvr, pow(10, cfg.num), cfg.interval, atm);
     anon_send(clnt, next_atom::value, size_t(0));
     system.await_all_actors_done();
   }
